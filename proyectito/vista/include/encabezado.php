629aba0d9ba3b58d8c5a7d15f3f03f986ba75928
Uriel Tejeiro <eleazartejeiro@gmail.com>
2020-07-29 22:44:30 -0400

    Materialize en proceso :s esta hardocore xD

--- proyectito/vista/include/encabezado.php
+++ proyectito/vista/include/encabezado.php
@@ -4,23 +4,53 @@
 	<meta charset="utf-8" />
 	<meta name="description" content="Website for Website Development Class-2019" />
 	<meta name="author" content="Hawkin Saeger" />
-	<link href='http://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css' />
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="vista/css/materialize.min.css"  media="screen,projection"/>
+  <!--Let browser know website is optimized for mobile-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 	<link rel="stylesheet" type="text/css" href="vista/css/estilos.css"/>
-	<title>Classroom Website</title>
+	<title>NUESTRO CLASSROOM</title>
 </head>
-
 <body>
 <div>
-	<div id="heading">
-		<img src="vista/images/logo.png" alt="logo.jpg" align="left" style="width:80px;height:80px;">
-		<h1>Hawkin's Classroom</h1>
-	</div>
-
-	<div class="nav_menu">
-		<ul>
-			<li><a href="index.php">Home</a></li>
-			<li><a href="estudianteInicio.php">Student</a></li>
-			<li><a href="profInicio.php">Tutor</a></li>
-			<li><a href="adminInicio.php">Admin</a></li>
-		</ul>
-	</div>
+	<div class="navbar-fixed">
+        <nav>
+            <div class="nav-wrapper deep-purple darken-2">
+                <!-- Logo -->
+                <img src="vista/images/logo.png" alt="logo.jpg" align="left" style="width:65px;height:65px;">
+								<a href="#" class="brand-logo center">NUESTRO PROPIO CLASSROOM</a>
+                <!-- Ícone para abrir no Mobile -->
+                <a href="index.php" data-target="mobile-navbar" class="sidenav-trigger">
+                    <i class="material-icons">menu</i>
+                </a>
+                <ul id="navbar-items" class="right hide-on-med-and-down">
+                    <li><a href="index.php">Inicio</a></li>
+                    <li>
+                        <a class="dropdown-trigger" data-target="dropdown-menu" href="#">
+                            Tipo de usuario <i class="material-icons right">arrow_drop_down</i>
+                        </a>
+                    </li>
+                </ul>
+                <!-- Dropdown -->
+                <ul id="dropdown-menu" class="dropdown-content">
+										<li><a href="controlador/estudianteInicio.php">Estudiante</a></li>
+										<li><a href="controlador/profInicio.php">Profesor</a></li>
+										<li><a href="controlador/adminInicio.php">Administrador</a></li>
+                </ul>
+            </div>
+						<!-- Menu Mobile -->
+				    <ul id="mobile-navbar" class="sidenav">
+								<li><a href="index.php">Inicio</a></li>
+								<li><a href="controlador/estudianteInicio.php">Estudiante</a></li>
+								<li><a href="controlador/profInicio.php">Profesor</a></li>
+								<li><a href="controlador/adminInicio.php">Administrador</a></li>
+				    </ul>
+        </nav>
+    </div>
+	<!--JavaScript at end of body for optimized loading-->
+  <script type="text/javascript" src="vista/js/materialize.min.js"></script>
+	<script type="text/javascript" src="vista/js/principal.js"></script>
+</body>
--- proyectito/vista/include/piePagina.php
+++ proyectito/vista/include/piePagina.php
@@ -1,7 +1,21 @@
 <!-- Footer for the Website -->
-<div id="footer">
-	<div>
-		<h3>&copy; Hawkin Saeger 2019</h3>
-		<p>Contact by email at saegerhawkin@gmail.com </p><br />
-	</div>
+<footer class="page-footer deep-purple darken-2">
+<div class="container">
+  <div class="row">
+    <div class="col l6 s12">
+      <h5 class="white-text">Grupo 2</h5>
+			<ul>
+        <li>Mauricio Arroyo</li>
+        <li>Alvaro Flores</li>
+        <li>Uriel Tejeiro</li>
+        <li>Anahi Yucra</li>
+      </ul>
+    </div>
+  </div>
 </div>
+<div class="footer-copyright">
+  <div class="container">
+  © 2020 Copyright Desarrollo de Aplicaciones Web
+  </div>
+</div>
+</footer>
