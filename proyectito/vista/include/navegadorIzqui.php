45b712f970a360220ecb9768cd46b6e2be3bee05
Uriel Tejeiro <eleazartejeiro@gmail.com>
2020-07-31 22:37:09 -0400

    css en camino falta el footer pinshi que se mueve

--- proyectito/vista/include/navegadorIzqui.php
+++ proyectito/vista/include/navegadorIzqui.php
@@ -8,10 +8,8 @@ if (session_status() === PHP_SESSION_NONE) {
 <!--Barra de navegacion izquierda-->
 <div class="row">
 	<div class="column side">
-        <ul class="menu">
 			<?php logInOut();
 			selectMenu();?>
-		</ul>
     </div>
 
 <?php
@@ -20,10 +18,10 @@ function logInOut()
 {
     //Funcion para mostrar ya sea el 'inicio de sesion' o el 'cerrar sesion' en la barra de navegacion lateral
     if (!isset($_SESSION['usuarioTipo'])) {
-        echo "<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/registrarse.php'>Registrarse</a></li>";
-        echo "<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/login.php'>Loguearse</a></li>";
+        echo "<a href='/proyectoDesarrolloWeb/proyectito/controlador/registrarse.php'>Registrarse</a>";
+        echo "<a href='/proyectoDesarrolloWeb/proyectito/controlador/login.php'>Loguearse</a>";
     } else {
-        echo "<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/cerrarSesion.php'>Cerrar sesion activa</a></li>";
+        echo "<a href='/proyectoDesarrolloWeb/proyectito/controlador/cerrarSesion.php'>Cerrar sesion activa</a>";
     }
 }
 
@@ -44,34 +42,34 @@ function selectMenu()
 function tutorMenu()
 {
     //Funcion para las opciones de los profesores
-    echo "<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/profInicio.php'>Pagina Principal</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/profUsuarioCalif.php'>Notas del estudiante</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/profNuevoCurso.php'>Añadir un nuevo curso</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/profCarga.php'>Subir un recurso</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/profAutorizaEstudiante.php'>Autorizar un estudiante</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/profAdicionaEstudiante.php'>Añadir estudiante</a></li>";
+    echo "<a href='/proyectoDesarrolloWeb/proyectito/controlador/profInicio.php'>Pagina Principal</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/profUsuarioCalif.php'>Notas del estudiante</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/profNuevoCurso.php'>Añadir un nuevo curso</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/profCarga.php'>Subir un recurso</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/profAutorizaEstudiante.php'>Autorizar un estudiante</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/profAdicionaEstudiante.php'>Añadir estudiante</a>";
 }
 
 function studentMenu()
 {
     //Funcion para las opciones del estudiante
-    echo "<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/estudianteInicio.php'>Pagina Principal</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/estudianteInscribirse.php'>Enlistarze en un curso</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/estudianteCurso.php'>Cursos</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/estudianteCalificaciones.php'>Notas</a></li>";
+    echo "<a href='/proyectoDesarrolloWeb/proyectito/controlador/estudianteInicio.php'>Pagina Principal</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/estudianteInscribirse.php'>Enlistarze en un curso</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/estudianteCurso.php'>Cursos</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/estudianteCalificaciones.php'>Notas</a>";
 }
 
 function adminMenu()
 {
     //Funcion para las opciones del administrador
-    echo "<li style='color:black; font-size:24px; font-weight:bold'>Enlaces de Administrador</li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/adminInicio.php'>Pagina Principal</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/adminAutoriza.php'>Autorizar usuarios</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/adminMuestraTablas.php'>Mostrar tablas de la base de datos</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/adminCreaTablas.php'>Crea tablas de la base de datos</a></li>
-		<li style='color:black; font-size:24px; font-weight:bold'>Enlaces del Profesor</li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/tutorShowUsers.php'>Mostrar Usuarios</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/profAutorizaEstudiante.php'>Autorizar estudiantes</a></li>
-		<li><a href='/proyectoDesarrolloWeb/proyectito/controlador/profAdicionaEstudiante.php'>Añadir estudiantes</a></li>";
+    echo "
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/adminInicio.php'>Pagina Principal</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/adminAutoriza.php'>Autorizar usuarios</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/adminMuestraTablas.php'>Mostrar tablas de la base de datos</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/adminCreaTablas.php'>Crea tablas de la base de datos</a>
+		
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/tutorShowUsers.php'>Mostrar Usuarios</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/profAutorizaEstudiante.php'>Autorizar estudiantes</a>
+		<a href='/proyectoDesarrolloWeb/proyectito/controlador/profAdicionaEstudiante.php'>Añadir estudiantes</a>";
 }
 ?>
