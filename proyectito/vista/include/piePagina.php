e6df47a55a038e675fc03e23d11b45b177941c0c
Uriel Tejeiro <eleazartejeiro@gmail.com>
2020-08-01 14:19:05 -0400

    gitignore funcionando

--- proyectito/vista/include/piePagina.php
+++
@@ -1,7 +0,0 @@
-<!-- Footer for the Website -->
-<div id="footer">
-	<div>
-		<h3>&copy; 2020 Copyright Desarrollo de Aplicaciones Web.</h3>
-		<p>Grupo 2: Mauricio Arroyo, Alvaro Flores, Uriel Tejeiro, Anahi Yucra</p> <br/>
-	</div>
-</div>
