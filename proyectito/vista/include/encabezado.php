e8ff5761747591847f70de016a004338f5fe4324
Uriel Tejeiro <eleazartejeiro@gmail.com>
2020-08-01 17:01:56 -0400

    agregado favicon a la pagina

--- proyectito/vista/include/encabezado.php
+++ proyectito/vista/include/encabezado.php
@@ -9,6 +9,7 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link href='http://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css' />
 	<link rel="stylesheet" type="text/css" href="/proyectoDesarrolloWeb/proyectito/vista/css/estilos.css"/>
+  <link rel="icon" href="/proyectoDesarrolloWeb/proyectito/vista/images/logo.png">
 	<title>Academy123</title>
 </head>
 <body>
